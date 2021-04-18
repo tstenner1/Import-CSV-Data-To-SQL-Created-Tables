d9b5a7f4780d862fbb2b659ea6dfd83f1f8093bf
Thomas Stenner <thomasstenner@thomass-mbp.attlocal.net>
2021-04-18 17:08:30 -0500

    new

--- data_engineering_tables.sql
+++ data_engineering_tables.sql
@@ -25,6 +25,17 @@ CREATE TABLE dept_emp (
 
 select * from dept_emp
 
+CREATE TABLE dept_managers (
+ dept_no VARCHAR,
+ emp_no INT,
+ from_date DATE NOT NULL,
+ to_date DATE NOT NULL,
+ FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+ FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+select * from dept_managers
+
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
