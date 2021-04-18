b8c5603f61bfd8df76fd15c1f09d51d34ba2ab79
Thomas Stenner <thomasstenner@thomass-mbp.attlocal.net>
2021-04-18 15:21:08 -0500

    new

--- data_analysis.sql
+++ data_analysis.sql
@@ -5,3 +5,23 @@ Salaries.salary
 FROM Employees
 INNER JOIN Salaries ON
 Employees.emp_no = Salaries.emp_no;
+
+List first name, last name, and hire date for employees who were hired in 1986.
+
+SELECT * FROM employees
+WHERE DATE_PART('year',hire_date) = 1986
+ORDER BY emp_no;
+
+
+List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
