1e24f12602e744a82d8550109ed0f4258699aa37
Thomas Stenner <thomasstenner@thomass-mbp.attlocal.net>
2021-04-18 16:55:20 -0500

    new

--- data_analysis.sql
+++ data_analysis.sql
@@ -63,6 +63,18 @@ WHERE departments.dept_name='Sales';
 
 List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
+SELECT 
+employees.emp_no, 
+employees.last_name, 
+employees.first_name,
+dept_emp.dept_no
+FROM employees 
+LEFT JOIN dept_emp 
+ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments 
+ON departments.dept_no=dept_emp.dept_no
+WHERE departments.dept_name in ('Sales', 'Development')
+
 In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
 SELECT last_name, COUNT(*) AS freq_count
