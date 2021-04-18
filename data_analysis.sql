1f80748dbe3a3572f9a559fb1565ada35cf2c311
Thomas Stenner <thomasstenner@thomass-mbp.attlocal.net>
2021-04-18 15:33:44 -0500

    new

--- data_analysis.sql
+++ data_analysis.sql
@@ -53,3 +53,7 @@ List all employees in the Sales and Development departments, including their emp
 
 In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
+SELECT last_name, COUNT(*) AS freq_count
+FROM employees
+GROUP BY last_name
+ORDER BY freq_count DESC;
