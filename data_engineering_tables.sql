c069433dadc6563177f4e4b7fdfd6d7bc8444f0e
Thomas Stenner <thomasstenner@thomass-mbp.attlocal.net>
2021-04-18 17:10:01 -0500

    new

--- data_engineering_tables.sql
+++ data_engineering_tables.sql
@@ -48,3 +48,12 @@ CREATE TABLE employees (
 
 select * from employees
 
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
+
+select * from salaries
